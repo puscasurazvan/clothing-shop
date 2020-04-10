e975a0d5291e89549664ec98cc4e8d8cf542a8f4
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-04-10 17:11:26 +0100

    split homepage into smaller components + styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import Homepage from './pages/homepage/homepage.component'
 
 function App() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
 }
 
 export default App
