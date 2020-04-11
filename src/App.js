53a0145c621555234fa6ba09eb045653b536ff39
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-04-11 16:34:23 +0100

    display collection items on the shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import { Switch, Route } from 'react-router-dom'
+
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   )
 }
