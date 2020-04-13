72f2c2f7d400ef7e7e26b45a23e57b0a0d198774
Razvan Puscasu <puscasurazvan93@gmail.com>
2020-04-13 15:09:28 +0100

    implement userReducer to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { connect } from 'react-redux'
 
+import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 
 import './header.styles.scss'
@@ -33,4 +33,8 @@ const Header = ({ currentUser }) => (
   </div>
 )
 
-export default Header
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+})
+
+export default connect(mapStateToProps)(Header)
